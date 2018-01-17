29215c1d5bb4ab907047cc0ce103cf2156fe348c
vdmkrnv <35402488+vdmkrnv@users.noreply.github.com>
2018-01-17 16:56:24 +0300

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -9,12 +10,8 @@ def index(request):
     return render(request, 'index.html')
 
 
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
